599bcdf8520f9202198036659936d53d705001c6
mikhail <mikhail@pop-os.localdomain>
2021-05-08 12:16:17 +0300

    Версия 4.5

---
+++ framework.py
@@ -0,0 +1,15 @@
+import selenium
+import allure
+
+
+@allure.step("Verify element {css_selector} on page.")
+def verify_element_presence(driver, css_selector):
+    try:
+        driver.find_element_by_css_selector(css_selector)
+    except selenium.common.exceptions.NoSuchElementException:
+        allure.attach(
+            name=driver.session_id,
+            body=driver.save_screenshot_as_png(),
+            attachment_type=allure.attachment_type.PNG
+        )
+        raise AssertionError(f"Element {css_selector} not found on page!")
